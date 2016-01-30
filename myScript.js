5513a747ddff7f53d7d785dfd29700706fdfdaf3
Skid Vis <skid.vis@agapered.com>
2016-01-30 16:01:59 -0600

    Add getty thumbnails. change short link urls.

--- myScript.js
+++ myScript.js
@@ -1,4 +1,7 @@
-﻿$(function () {
+﻿var shortlink; 
+var search;
+
+$(function () {
     $("#refresh").bind("click", function () { doTag(1) });
     doTag(0);
 });
@@ -9,12 +12,12 @@ function doTag(fresh) {
     verbs = ["fishing with", "eating", "chasing", "talking to", "beating up", "drinking with", "firing", "riding on", "shooting", "calling", "fighting", "racing", "arresting", "bathing", "becoming", "cooking", "cheering", "choking", "catching", "carrying", "dancing with", "decorating", "controlling", "driving", "feeding", "filming", "guarding", "haunting", "licking", "preaching to", "shocking", "throwing", "washing"];
     myNumbers = [];
 
-    if (querystring("themer") === "" || fresh === 1) {
+    if (querystring() === "" || fresh === 1) {
         myNumbers = [randomize(adjectives.length), randomize(nouns.length), randomize(verbs.length), randomize(adjectives.length), randomize(nouns.length)]
         if (myNumbers[0] === myNumbers[3]) { myNumbers[3] = randomize(adjectives.length); }
         if (myNumbers[1] === myNumbers[4]) { myNumbers[4] = randomize(nouns.length); }
     } else {
-        myString = querystring("themer");
+        myString = querystring("");
 		if(myString.indexOf("-")>-1){
 			myNumbers = myString.split("-");
 		}else{
@@ -22,17 +25,58 @@ function doTag(fresh) {
 		}
     }
     
-    $("#tagline").text("a " + adjectives[myNumbers[0]] + " " + nouns[myNumbers[1]] + " " + verbs[myNumbers[2]] + " a " + adjectives[myNumbers[3]] + " " + nouns[myNumbers[4]]);
-    $("#myLink").attr("href", "/?themer=" + myNumbers.join("a"));
+    var adj1 = adjectives[myNumbers[0]];
+    var noun1 = nouns[myNumbers[1]];
+    var verb = verbs[myNumbers[2]];
+    var adj2 = adjectives[myNumbers[3]];
+    var noun2 = nouns[myNumbers[4]];
+
+    $("#tagline").text("a " + adj1 + " " + noun1 + " " + verb + " a " + adj2 + " " + noun2);
+    $('#adj1').text(adj1);
+    $('#noun1 span').text(noun1);
+    get_image(noun1, '#noun1');
+    $('#verb').text(verb);
+    $('#adj2').text(adj2);
+    $('#noun2 span').text(noun2 + '.');
+    //$('#noun2 img').attr('src', 'http://blog.comperiosearch.com/wp-content/uploads/2012/09/handlebars_logo-150x150.png');
+    get_image(noun2, '#noun2');
+    
+    shortlink = "/?" + myNumbers.join("-");
+    $("#myLink").attr("href", shortlink);
+    $("#facebook_link").attr("href", "https://www.facebook.com/sharer/sharer.php?u=http://www.drawlly.com" + shortlink);
+    $("#twitter_link").attr("href", "https://twitter.com/intent/tweet?text=http://www.drawlly.com" + shortlink);
 }
 
 function randomize(max) {
     return Math.floor(Math.random() * max);
 }
 
-function querystring(key) {
-    var re = new RegExp('(?:\\?|&)' + key + '=(.*?)(?=&|$)', 'gi');
+function querystring() {
+    var re = new RegExp('(?:\\?|&)' + '(.*?)(?=&|$)', 'gi');
     var r = [], m;
     while ((m = re.exec(document.location.search)) != null) r.push(m[1]);
     return r.toString();
 }
+
+function get_image(keyword, img){
+        $.ajax({
+          url: 'https://api.gettyimages.com:443/v3/search/images/creative?phrase=' + keyword,
+          type: 'GET',
+          dataType: 'json',
+          success: function(data){ got_image(data, img) },
+          error: function() { alert('boo!'); },
+          beforeSend: setHeader
+        });
+}
+
+function got_image(data, img){
+    var image_number = randomize(data.images.length)
+    $(img + ' img').attr('src', data.images[image_number].display_sizes[0].uri) ;
+}
+
+function setHeader(xhr) {
+    xhr.setRequestHeader('Api-Key', 'gu4mn7ukacpafbqu9mydqfez');
+}
+
+
+
