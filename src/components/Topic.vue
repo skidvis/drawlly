19f13472a64410775cc6a35b0e5f12fd6e8e4cd4
Skid Vis <skid@skidvis.com>
2025-08-02 17:41:23 -0500

    move to astro

---
+++ src/components/Topic.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        <div class="tagline">
+            <ul>
+                <li>A</li>
+                <li id="adj1">adjective</li>
+                <li id="noun1" class="noun">
+                    <span>hi</span>
+                </li>
+                <li id="verb">verb</li>
+                <li>a</li>
+                <li id="adj2">adjective</li>
+                <li id="noun2" class="noun">
+                    <span>bye</span>
+                </li>
+            </ul>
+        </div>
+        <h5><a href="#" id="refresh">refresh</a> -- <a id="myLink" href="#">permalink</a></h5>        
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
